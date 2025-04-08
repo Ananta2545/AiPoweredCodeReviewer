289e665633e83bb8aa73d0d1b5bb132a3291d448
Ananta2545 <chatterjeeanata091@gmail.com>
2025-04-08 18:45:59 +0530

    updated link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ function App() {
   const reviewCode = async()=>{
     setLoading(true);
     try{
-      const response = await axios.post('http://localhost:3000/ai/get-review', { code });
+      const response = await axios.post('https://aipoweredcodereviewer.onrender.com/ai/get-review', { code });
       setReview(response.data);
     }catch(err){
       console.error("Error fetching review:", err);
